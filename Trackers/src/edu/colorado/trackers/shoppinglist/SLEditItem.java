87fcba365b674818539f4414be973d9c085c00e6
ben <ben@chauncey>
2012-12-04 16:02:01 -0700

    Added +/- widgit to shopping list

--- Trackers/src/edu/colorado/trackers/shoppinglist/SLEditItem.java
+++ Trackers/src/edu/colorado/trackers/shoppinglist/SLEditItem.java
@@ -11,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import edu.colorado.trackers.R;
 
 public class SLEditItem extends Activity {
@@ -18,11 +19,13 @@ public class SLEditItem extends Activity {
 	private ShoppingListDB shoppingListDB = new ShoppingListDB(this);
 	private Button cancelButton;
 	private Button okButton;
+	private Button minusButton;
+	private Button plusButton;
 	private boolean saveData = true;
 	private Integer itemId = -1;
 	private String itemName = "";
 	private Double itemPrice = 0.0;
-	private Integer itemQuantity = 0;
+	private Integer itemQuantity = 1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,8 @@ public class SLEditItem extends Activity {
         
         cancelButton = (Button) findViewById(R.id.cancel_button);
         okButton = (Button) findViewById(R.id.ok_button);
+        minusButton = (Button) findViewById(R.id.minus_button);
+        plusButton = (Button) findViewById(R.id.plus_button);
         
         cancelButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
@@ -45,6 +50,22 @@ public class SLEditItem extends Activity {
 			public void onClick(View v) {
 				saveData = true;
 				finish();
+			}
+		});
+        minusButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				TextView quantity = (TextView) findViewById(R.id.sl_quantity);
+				Integer value = Integer.parseInt(quantity.getText().toString());
+				value -= (value > 0) ? 1 : 0;
+				quantity.setText(value.toString());
+			}
+		});
+        plusButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				TextView quantity = (TextView) findViewById(R.id.sl_quantity);
+				Integer value = Integer.parseInt(quantity.getText().toString());
+				value += (value < 99) ? 1 : 0;
+				quantity.setText(value.toString());
 			}
 		});
     }
@@ -71,6 +92,10 @@ public class SLEditItem extends Activity {
     	super.onResume();
     	saveData = true;
     	
+    	EditText editName = (EditText) findViewById(R.id.sl_name);
+        EditText editPrice = (EditText) findViewById(R.id.sl_price);
+        TextView textQuantity = (TextView) findViewById(R.id.sl_quantity);
+    	
     	if (itemId != -1) {
         	SQLiteDatabase db = shoppingListDB.getReadableDatabase();
         	Cursor cursor = db.query("shopping_list_items", 
@@ -82,15 +107,11 @@ public class SLEditItem extends Activity {
         	itemPrice = cursor.getDouble(2);
         	itemQuantity = cursor.getInt(3);
         	cursor.close();
-        	
-        	EditText editName = (EditText) findViewById(R.id.sl_name);
-            EditText editPrice = (EditText) findViewById(R.id.sl_price);
-            EditText editQuantity = (EditText) findViewById(R.id.sl_quantity);
-            
-            editName.setText(itemName);
-            editPrice.setText(itemPrice.toString());
-            editQuantity.setText(itemQuantity.toString());
-        }
+    	}
+    	
+        editName.setText(itemName);
+        editPrice.setText(itemPrice.toString());
+        textQuantity.setText(itemQuantity.toString());
     }
     
     @Override
@@ -102,11 +123,11 @@ public class SLEditItem extends Activity {
 			
 			EditText nameEdit = (EditText) findViewById(R.id.sl_name);
 			EditText priceEdit = (EditText) findViewById(R.id.sl_price);
-			EditText quantityEdit = (EditText) findViewById(R.id.sl_quantity);
+			TextView quantityText = (TextView) findViewById(R.id.sl_quantity);
 			
 			String name = nameEdit.getText().toString();
 			String priceString = priceEdit.getText().toString();
-			String quantityString = quantityEdit.getText().toString();
+			String quantityString = quantityText.getText().toString();
 			Double price = Double.parseDouble(priceString);
 			Integer quantity = Integer.parseInt(quantityString);
 			
