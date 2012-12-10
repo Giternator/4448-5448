20e67afa09012156d783ad9ef526f781cf5cb8eb
reachdvr <reachdvr@gmail.com>
2012-12-10 13:43:58 -0700

    deleted old files

--- Trackers/src/edu/colorado/trackers/HealthMetrics/CheckInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.healthmetrics;
-
-import android.content.Context;
-
-
-interface CheckInterface 
-{
-	int check(Context con, int age);
-}
--- Trackers/src/edu/colorado/trackers/HealthMetrics/Metric.java
+++
@@ -1,8 +0,0 @@
-package com.example.healthmetrics;
-
-public abstract class Metric implements CheckInterface{
-
-	
-	
-	
-}
